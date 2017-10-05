bcba986e43e52b54cea4c293f810b230d69a111a
Thierry Bouquain <thierry.bouquain@ucaya.com>
2017-10-05 19:06:17 +0200

    add advertise ext ip

--- onbuild/entrypoint.sh
+++ onbuild/entrypoint.sh
@@ -4,6 +4,7 @@ set -e
 if [ ! -z ${EVENTSTORE_CLUSTER_SIZE+x} ]
 then
     export EVENTSTORE_INT_IP=`ip addr show eth0|grep '\/24'|awk '/inet / {print $2}'|sed -e 's/\/.*//'`
+    export EVENTSTORE_EXT_IP_ADVERTISE_AS=`ip addr show eth0|grep '\/24'|awk '/inet / {print $2}'|sed -e 's/\/.*//'`
 fi
 
 exec eventstored
